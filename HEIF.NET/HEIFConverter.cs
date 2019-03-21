edb16015687ea1d8beeb67fc4a5368667799f20f
Tom Needham <06needhamt@gmail.com>
2019-03-21 21:38:41 +0000

    Working HEIF Decoder With Customisable Quality

--- HEIF.NET/HEIFConverter.cs
+++ HEIF.NET/HEIFConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,61 @@ namespace HEIF.NET
 {
     public class HEIFConverter
     {
+        public string InputFile { get; private set; }
+        public string OutputFile { get; private set; }
+        public int Quality { get; private set; }
 
+        public HEIFConverter(string inputFile, string outputFile, int quality)
+        {
+            quality = quality.Clamp(1, 100);
+            this.InputFile = inputFile;
+            this.OutputFile = outputFile;
+            this.Quality = quality;
+        }
+
+        public bool ConvertHEIFToJPG()
+        {
+            try
+            {
+                string tempFile = "temp_file.hief";
+                int copySize = 0;
+                byte[] heifImage = NativeBridge.read_heif(InputFile);
+                byte[] buffer;
+
+                buffer = NativeBridge.invoke_heif2jpg(heifImage, Quality, tempFile, ref copySize, true, true);
+                FileStream fs = new FileStream(OutputFile, FileMode.Create);
+                BinaryWriter writer = new BinaryWriter(fs);
+
+                try
+                {
+                    writer.Write(buffer, 0, copySize);
+                    writer.Close();
+                    fs.Close();
+                    File.Delete(tempFile);
+                }
+                catch (Exception ex)
+                {
+                    try
+                    {
+                        Console.WriteLine(string.Format("Error Occurred While writing HEIF Buffer {0}", ex));
+                        writer.Close();
+                        fs.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(string.Format("Error Occurred While Closing HEIF Buffer {0}", e));
+                        return false;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Error Occurred While Converting HEIF To JPG {0}", ex));
+                return false;
+            }
+
+            return true;
+        }
     }
 }
---
+++ TestApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using HEIF.NET;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> heifFiles = Directory.EnumerateFiles(Environment.CurrentDirectory).ToList();
+            string outputDirectory = Path.Combine(Environment.CurrentDirectory, "outputs");
+            string qualityDirectory = string.Empty;
+
+            int quality = 100;
+
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Usage: [Quality]<0-100>");
+                return;
+            }
+
+            if(!int.TryParse(args[0], out quality))
+            {
+                Console.WriteLine("A Decoding Quality Between 1 and 100 must be specified");
+                return;
+            }
+
+            if (!Directory.Exists(outputDirectory))
+                Directory.CreateDirectory(outputDirectory);
+
+            qualityDirectory = Path.Combine(outputDirectory, quality.ToString());
+
+            if (Directory.Exists(qualityDirectory))
+                Directory.Delete(qualityDirectory, true);
+
+            Directory.CreateDirectory(qualityDirectory);
+
+            foreach(string inputFile in heifFiles)
+            {
+                if (inputFile.Contains("temp_file"))
+                    continue;
+                Console.WriteLine(string.Format("Converting Image {0} to JPG", Path.GetFileNameWithoutExtension(inputFile)));
+                string outputFile = Path.Combine(qualityDirectory, Path.GetFileNameWithoutExtension(inputFile) + ".jpg");
+                HEIFConverter converter = new HEIFConverter(inputFile, outputFile, quality);
+                if (!converter.ConvertHEIFToJPG())
+                    Console.WriteLine(string.Format("Error Converting Image {0} to JPG", Path.GetFileNameWithoutExtension(inputFile)));
+                Console.WriteLine(string.Format("Successfully Converted Image {0} to JPG", Path.GetFileNameWithoutExtension(inputFile)));
+            }
+        }
+    }
+}
